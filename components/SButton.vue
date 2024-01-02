57adecd36dbf335f450546de40e401b7d5754eff
Farid <56141667+farid-temuri@users.noreply.github.com>
2024-01-02 05:09:48 +0300

    feat: add tailwind and typed router and main page to show it

--- components/SButton.vue
+++ components/SButton.vue
@@ -1,13 +1,21 @@
 <template>
-	<buttton class="bg-slate-100 border-[1px] rounded-[4px] hover:bg-slate-400  text-slate-700 border-slate-700 transition">
+	<buttton class="bg-slate-100 border-[1px] rounded-[4px] hover:bg-slate-400  text-slate-700 border-slate-700 transition" :class="{
+		active: active,
+		nigger: !active
+	}">
 		<slot></slot>
 	</buttton>
 </template>
 
 <script lang='ts' setup>
-const props = defineProps({
-	
-} )
+const props = defineProps<{ active: boolean }>()
+
 console.log(props);
 
 </script>
+
+<style scoped>
+.active{
+	background-color: red!important;
+}
+</style>
